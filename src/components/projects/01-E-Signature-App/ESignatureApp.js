7095e31f3573d16d0baa8bc9878a82dc93289f48
DreasWeiss <dreasweiss@gmail.com>
2023-11-07 17:48:17 +0300

    add scss styles

--- src/components/projects/01-E-Signature-App/ESignatureApp.js
+++ src/components/projects/01-E-Signature-App/ESignatureApp.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function ESignatureApp() {
     return (
-        <div className="container">
-            <h1>hello world</h1>
+        <div className="ESignatureApp">
+            <div className="container">
+                <div className="up">
+                    <div className="title">LAURA</div>
+                    <div className="title">LAURA</div>
+                    <div className="decription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel modi consequuntur deleniti nulla voluptate ad necessitatibus eligendi, illum nesciunt odio perspiciatis eius repudiandae, veritatis ea nostrum facilis odit voluptas. Ipsa.</div>
+                </div>
+                <div className="down">
+                    <input type="text" />
+                    <input type="text" />
+                </div>
+            </div>
+
         </div>
     )
 }
