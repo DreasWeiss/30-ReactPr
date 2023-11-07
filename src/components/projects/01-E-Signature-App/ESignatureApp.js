ae699c154b963cfdc35facee3b2aff0e9f67257b
DreasWeiss <dreasweiss@gmail.com>
2023-11-07 19:01:08 +0300

    01 project ESignatureApp - done

--- src/components/projects/01-E-Signature-App/ESignatureApp.js
+++ src/components/projects/01-E-Signature-App/ESignatureApp.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../components/Title'
 
 export default function ESignatureApp() {
+    const [name, setName] = useState('Ur Signature');
+    const [date, setDate] = useState('');
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleDateChange = (e) => {
+        setDate(e.target.value);
+    }
+
+    const inputStyle = {
+        border: 'none',
+        borderBottom: '2px dotted',
+        outline: 'none',
+        padding: '.35rem 0'
+    }
+    document.body.style.background = 'rgba(201, 189, 189, 0.7)';
     return (
         <div className="ESignatureApp">
             <div className="container">
                 <div className="up">
-                    <div className="title">LAURA</div>
-                    <div className="title">LAURA</div>
-                    <div className="decription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel modi consequuntur deleniti nulla voluptate ad necessitatibus eligendi, illum nesciunt odio perspiciatis eius repudiandae, veritatis ea nostrum facilis odit voluptas. Ipsa.</div>
+                    <Title text={name} />
+                    <Title classes={'subtitle'} text={!date ? 'DoB' : date} />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sit neque ea deleniti iusto beatae. Temporibus dolore ipsa rerum adipisci quo doloremque quod nostrum deserunt non iusto atque, consequatur eaque corporis corrupti veniam distinctio quibusdam facere sequi ullam sit est itaque omnis? Quasi provident eius nemo, natus omnis corporis dolorem veritatis modi laboriosam corrupti, sequi, nisi in id laborum officia quibusdam debitis nostrum vitae placeat enim voluptatibus repellendus iste? Molestiae maiores aspernatur rem provident facere dignissimos pariatur amet repudiandae commodi!</p>
                 </div>
                 <div className="down">
-                    <input type="text" />
-                    <input type="text" />
+                    <input
+                        type="date"
+                        value={date}
+                        style={inputStyle}
+                        onChange={handleDateChange} />
+                    <input
+                        type="text"
+                        value={name}
+                        style={inputStyle}
+                        onChange={handleNameChange} />
                 </div>
             </div>
 
