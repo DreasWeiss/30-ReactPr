d8be4246357b408f4398f9ac02623363592811a7
DreasWeiss <dreasweiss@gmail.com>
2023-11-08 14:09:29 +0300

    02 Randomize Colors - starter pack

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,16 @@
-import ESignatureApp from '../projects/01-E-Signature-App/ESignatureApp';
 import './App.scss';
 
 //projects
+// import ESignatureApp from '../projects/01-E-Signature-App/ESignatureApp';
+import RandomizeColors from '../projects/02-Randomize-Colors/RandomizeColors';
 
 
 function App() {
   return (
     <div className="App">
-      <ESignatureApp />
+      {/* <ESignatureApp /> */}
+      <RandomizeColors />
+
     </div>
   );
 }
