6140c3a8be7f1a3eaa9bfdc2799216a451e69e10
DreasWeiss <dreasweiss@gmail.com>
2023-11-13 19:10:00 +0300

    add Button component

--- src/components/projects/components/Button.jsx
+++ src/components/projects/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Button({ text }) {
+export default function Button({ text, btnClass }) {
     return (
-        <button className={`btn `}> {!text ? 'click' : text}
+        <button className={`btn ${btnClass}`}> {!text ? 'click' : text}
 
         </button>
     )
