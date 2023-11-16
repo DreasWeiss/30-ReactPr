4d858b83639fb485d1856ccdd5c35b0e1bd9e332
DreasWeiss <dreasweiss@gmail.com>
2023-11-16 23:41:08 +0300

    temperature controller - done

--- src/components/projects/06-Temperature-Controller/TemperatureController.jsx
+++ src/components/projects/06-Temperature-Controller/TemperatureController.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import Button from '../components/Button'
+import { RiCelsiusFill } from "react-icons/ri";
 
 export default function TemperatureController() {
+    const [temperature, setTemperature] = useState(0);
+
+    const increaseTemperature = (e) => {
+        setTemperature(temperature + 1);
+    }
+
+    const decreaseTemperature = (e) => {
+        setTemperature(temperature - 1);
+    }
+
     return (
         <div className='container mt-3 text-center'>
             <Title text={'Temperature Controller'} />
@@ -10,24 +21,27 @@ export default function TemperatureController() {
                 className="card bg-light m-auto"
                 style={{ width: 200 }}>
                 <h1
-                    className='text-light card border-50'
+                    className={`text-light card border-50 ${temperature > 0 ? 'bg-danger' : 'bg-info'}`
+                    }
                     style={
                         {
                             height: 150,
                             width: 150,
-                            border: '2px solid #666'
+                            border: '2px solid #666',
+                            display: 'flex',
+                            flexDirection: 'row'
                         }}>
-                    Temperature
+                    {temperature} <RiCelsiusFill style={{ marginLeft: 5 }} />
                 </h1>
                 <div className="d-flet my-2">
                     <Button
                         text={'-'}
                         btnClass={'btn-lg'}
-                        onClick={() => console.log('-')} />
+                        onClick={decreaseTemperature} />
                     <Button
                         text={'+'}
                         btnClass={'btn-lg'}
-                        onClick={() => console.log('+')} />
+                        onClick={increaseTemperature} />
                 </div>
             </div>
         </div>
