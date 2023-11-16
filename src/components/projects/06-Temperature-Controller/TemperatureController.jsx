d0ed486640e2c50dcd2206d73601188ad448d8fa
DreasWeiss <dreasweiss@gmail.com>
2023-11-16 23:24:32 +0300

    temperature controller - body

--- src/components/projects/06-Temperature-Controller/TemperatureController.jsx
+++ src/components/projects/06-Temperature-Controller/TemperatureController.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Title from '../components/Title'
+import Button from '../components/Button'
 
 export default function TemperatureController() {
     return (
-        <div>
-            <h1>Hello world</h1>
+        <div className='container mt-3 text-center'>
+            <Title text={'Temperature Controller'} />
+            <div className="card bg-light m-auto" style={{ width: 200 }}>
+                <h1
+                    className='text-light card border-50'
+                    style={
+                        { height: 150, width: 150, border: '2px solid #666' }} >Temperature
+                </h1>
+                <div className="d-flet my-2">
+                    <Button text={'-'} btnClass={'btn-lg'} onClick={() => console.log('-')} />
+                    <Button text={'+'} btnClass={'btn-lg'} onClick={() => console.log('+')} />
+                </div>
+            </div>
         </div>
     )
 }
