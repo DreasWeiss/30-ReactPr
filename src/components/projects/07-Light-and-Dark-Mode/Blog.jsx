cf3f7d991ebc9f840fe65fbe4d58f82849424489
DreasWeiss <dreasweiss@gmail.com>
2023-11-18 22:56:08 +0300

    Light and Dark Mode - done

--- src/components/projects/07-Light-and-Dark-Mode/Blog.jsx
+++ src/components/projects/07-Light-and-Dark-Mode/Blog.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from '../components/Button'
 import Title from '../components/Title'
+import { ThemeContext } from './contex/theme-contex';
 
-export default function Blog({ theme }) {
+export default function Blog() {
+    const { theme, changeTheme } = useContext(ThemeContext);
+
     return (
-        <div className='container'>
+        <div className='container p-1'>
             <Title text={`My Blog with ${theme} Theme`} />
             <span style={{ position: 'absolute', top: 10, right: 10 }}>
-                <Button text={theme} onClick={() => console.log('click')} />
+                <Button
+                    text={theme === 'dark' ? 'Light' : 'Dark'}
+                    btnClass={`${theme === 'dark' && 'btn-light'} btn-sm`}
+                    onClick={changeTheme} />
             </span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, earum. Asperiores sed accusantium fugiat voluptatem, dicta quasi fugit veniam ad repudiandae blanditiis pariatur quas reprehenderit. Minima eum doloribus facilis quibusdam.</p>
         </div>
--- src/components/projects/07-Light-and-Dark-Mode/LightDarkMode.jsx
+++ src/components/projects/07-Light-and-Dark-Mode/LightDarkMode.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Blog from './Blog'
+import { ThemeContext, themes } from './contex/theme-contex';
 
 
 export default function LightDarkMode() {
 
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(themes.light);
+
+    useEffect(() => {
+        let docBody = document.body;
+        switch (theme) {
+            case themes.light:
+                docBody.classList.remove('bg-dark');
+                docBody.classList.remove('text-light');
+                docBody.classList.add('bg-light');
+                docBody.classList.add('text-dark');
+                break;
+            case themes.dark:
+                docBody.classList.remove('bg-light');
+                docBody.classList.remove('text-dark');
+                docBody.classList.add('bg-dark');
+                docBody.classList.add('text-light');
+        }
+    }, [theme]);
+
+    function changeTheme() {
+        // console.log(theme);
+
+        theme === themes.light ?
+            setTheme(themes.dark) :
+            setTheme(themes.light);
+    }
 
     return (
-        <div className='container p-1'>
+        <ThemeContext.Provider value={{ theme, changeTheme }}>
             <Blog theme={theme} />
-        </div>
+        </ThemeContext.Provider>
     )
 }
