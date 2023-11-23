e3da3d1de84918f819ea8ce88109b48280538938
DreasWeiss <dreasweiss@gmail.com>
2023-11-23 16:36:48 +0300

    Lock screem prepairing - done

--- src/components/projects/08-Slide-To-Unlock/SlideToUnlock.jsx
+++ src/components/projects/08-Slide-To-Unlock/SlideToUnlock.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 import LockSlider from './LockSlider'
 import { AiFillUnlock } from 'react-icons/ai'
+import LockScreenImg from './img/road.jpg';
+import HomeScreenImg from './img/beach.jpg';
 
 export default function SlideToUnlock() {
+    const [uiProps, setUiProps] = useState({
+        uiText: 'Unlock screen',
+        uiColor: '#eee',
+        uiBg: `url(${LockScreenImg}) center/cover no-repeat`
+    });
     const [showLockSlider, setShowLockSlider] = useState(true);
     return (
-        <div className='container text-center d-flex flex-column border-20' style={{
+        <div className='container text-center d-flex flex-column border-20 shadow-md' style={{
             height: '70vh',
             marginTop: '15vh',
             width: 340,
-            border: '4px solid #000'
+            border: '4px solid #000',
+            background: uiProps.uiBg
         }}>
-            <h1 className="title">title</h1>
+            <h1 className="title" style={{ color: uiProps.uiColor }}>{uiProps.uiText}</h1>
             {showLockSlider ? (
                 <LockSlider width={'250px'} />
             ) :
