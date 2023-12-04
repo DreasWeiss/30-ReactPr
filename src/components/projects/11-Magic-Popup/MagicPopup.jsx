422b66f56b49322963154263a937f63f3c9d70e4
DreasWeiss <dreasweiss@gmail.com>
2023-12-04 15:58:20 +0300

    MagicPopup - manual func done

--- src/components/projects/11-Magic-Popup/MagicPopup.jsx
+++ src/components/projects/11-Magic-Popup/MagicPopup.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import Button from '../components/Button'
 import Popup from '../components/Popup'
 
 export default function MagicPopup() {
+
+    const [trigger, setTrigger] = useState(false);
+
+    const triggerPopup = () => {
+        setTrigger(true);
+    }
+
     return (
         <div className='container text-center'>
             <Title text={'Magic Popup APP'} />
-            <Button text={'Click me'} />
-            <Popup type={'alert-info'} />
+            <Title
+                text={'click for popup or wait for 3 seconds'}
+                classes={'subtitle'} />
+            <Button
+                text={'Click me'}
+                btnClass={'btn-primary'}
+                onClick={triggerPopup} />
+            {trigger &&
+                <Popup
+                    type={'alert-info'}
+                    title={'Triggered PopUP :)'}
+                    text={'This popup was triggered by button'}
+                    handleClose={setTrigger} />
+            }
         </div>
     )
 }
--- src/components/projects/components/Popup.jsx
+++ src/components/projects/components/Popup.jsx
@@ -10,27 +10,27 @@ export default function Popup({ type, title, text, handleClose }) {
         height: '100vh',
         width: '100vw',
         background: 'rgba(0,0,0,.1)',
-        zIndex: '-1'
+        zIndex: '1'
     }
 
     const popupStyle = {
         position: 'relative',
         margin: '40vh auto',
-        zIndex: '1'
+        zIndex: '2'
     }
 
     return (
         <div style={popupContainer}>
             <div className={`${type}`} style={popupStyle}>
                 <div className="alert-close">
-                    <div className="d-flex flex-column">
-                        <h4 className="mb-4">{title && title}</h4>
+                    <div className="d-flex flex-column text-left">
+                        <h4 className="mb-1">{title && title}</h4>
                         <p>{text && text}</p>
                     </div>
                     <Button
                         btnClass={'btn-close'}
                         text={'x'}
-                        onClick={handleClose} />
+                        onClick={() => handleClose(false)} />
                 </div>
             </div>
         </div>
