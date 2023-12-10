27dbe796fcb99cb55929d09f83a656cf93c735fb
DreasWeiss <dreasweiss@gmail.com>
2023-12-10 19:51:11 +0300

    Dialog Box - starter Pack

--- src/components/projects/13-Dialog-Box/DialogBox.jsx
+++ src/components/projects/13-Dialog-Box/DialogBox.jsx
@@ -4,6 +4,9 @@ export default function DialogBox() {
     return (
         <div>
             <h1>hello world</h1>
+            <h1>hello world</h1>
+            <h1>hello world</h1>
+
         </div>
     )
 }
