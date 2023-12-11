55b572f2f1eb2f9316c297aa92f435b8a3252296
DreasWeiss <dreasweiss@gmail.com>
2023-12-11 18:50:06 +0300

    DialogBox - structure

--- src/components/projects/13-Dialog-Box/DialogBox.jsx
+++ src/components/projects/13-Dialog-Box/DialogBox.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import Button from '../components/Button'
 
-export default function DialogBox() {
+export default function DialogBox({ width = 350 }) {
     return (
-        <div>
-            <h1>hello world</h1>
-            <h1>hello world</h1>
-            <h1>hello world</h1>
-
+        <div className='card bg-light m-auto mt-4' style={{ width: width }}>
+            <div className="card-body">
+                <div
+                    className="d-grid"
+                    style={{
+                        gridTemplateColumns: '2fr 1fr',
+                        alignItems: 'center',
+                        alignSelf: 'center',
+                        justifyContent: 'center'
+                    }}>
+                    <section>
+                        <h2 className="card-title">title</h2>
+                        <h3 className="card-text">text</h3>
+                    </section>
+                    <div className="">icon</div>
+                </div>
+            </div>
+            <hr />
+            <div className="d-flex mb-2 px-1 text-end">
+                <Button text={'Cancel'} btnClass={'btn-light'} />
+                <Button text={'Subscribe'} btnClass={'btn-danger'} />
+                <Button text={'Unsubscribe'} btnClass={'btn-danger btn-block'} />
+            </div>
         </div>
     )
 }
