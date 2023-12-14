7db4bad0a83b59dadd33244285d85082038d077f
DreasWeiss <dreasweiss@gmail.com>
2023-12-14 18:25:10 +0300

    add data

--- src/components/projects/14-Search-Filter-App/SearchFilterApp.jsx
+++ src/components/projects/14-Search-Filter-App/SearchFilterApp.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react'
 import Title from '../components/Title'
 
+import ContactsData from './Data.json'
+
 export default function SearchFilterApp() {
 
     let inputSearch = useRef(null);
@@ -19,6 +21,11 @@ export default function SearchFilterApp() {
             <section
                 className="d-flex"
                 style={{ gap: 15, maxWidth: '1600px', margin: 'auto' }}>
+                {ContactsData.map((contact) => (
+                    <ul>
+                        <li>{contact.last_name}</li>
+                    </ul>
+                ))}
 
             </section>
         </div>
