f8891f63486e9d4f7ef456869329396c3ddf1f74
DreasWeiss <dreasweiss@gmail.com>
2023-12-14 14:00:59 +0300

    create input with autofocus

--- src/components/projects/14-Search-Filter-App/SearchFilterApp.jsx
+++ src/components/projects/14-Search-Filter-App/SearchFilterApp.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import Title from '../components/Title'
 
 export default function SearchFilterApp() {
+
+    let inputSearch = useRef(null);
+    useEffect(() => {
+        inputSearch.current.focus();
+    })
     return (
-        <div>
-            <h1>hello world</h1>
+        <div className='container text-center'>
+            <Title text={'Search Filter APP'} />
+            <input
+                type="text"
+                placeholder='Search by first name...' c
+                className='mb-2'
+                style={{ padding: '0.3rem 0.5rem' }}
+                ref={inputSearch} />
+            <section
+                className="d-flex"
+                style={{ gap: 15, maxWidth: '1600px', margin: 'auto' }}>
+
+            </section>
         </div>
     )
 }
