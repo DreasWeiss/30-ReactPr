6a7f5d04690010d8eccd9e563d4d7066c7177ba0
DreasWeiss <dreasweiss@gmail.com>
2024-01-18 20:23:53 +0300

    restructure to the comp-s

--- src/components/projects/16-Notes-App/NotesApp.jsx
+++ src/components/projects/16-Notes-App/NotesApp.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import NewNote from './NewNote'
+import Button from '../components/Button'
 
 export default function NotesApp() {
     return (
-        <div>
-            <h1>Notes App</h1>
+        <div className='container'>
+            <Button btnClass={'btn-info'} text={'+ new note'} />
+            <div
+                className="container container-sm d-flex"
+                style={{ gap: 25, flexWrap: 'wrap' }}>
+                <NewNote />
+            </div>
         </div>
     )
 }
