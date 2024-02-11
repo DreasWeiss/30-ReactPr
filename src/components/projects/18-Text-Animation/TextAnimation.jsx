09fbc0a3b45e370c7ac672037debae00bd5a318e
DreasWeiss <dreasweiss@gmail.com>
2024-02-11 18:43:58 +0300

    add ui style

--- src/components/projects/18-Text-Animation/TextAnimation.jsx
+++ src/components/projects/18-Text-Animation/TextAnimation.jsx
@@ -17,3 +17,22 @@ export default function TextAnimation() {
         </div>
     )
 }
+import React from 'react'
+import Button from '../components/Button'
+
+export default function TextAnimation() {
+    return (
+        <div className='container text-center'>
+            <form
+                action=""
+                className="container d-flex mt-4"
+                onSubmit={(e) => e.preventDefault()}>
+                <div className="form-group">
+                    <label htmlFor="">Type in your text to be animated</label>
+                    <input type="text" placeholder='Your text' />
+                </div>
+                <Button text={'Clear'} btnClass={'btn-large btn-danger'} />
+            </form>
+        </div>
+    )
+}
